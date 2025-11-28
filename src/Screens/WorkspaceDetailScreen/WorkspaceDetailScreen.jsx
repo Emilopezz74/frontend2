6562f9fc3b4d0de2f8d915920cc60b0f5507a3cf
Emilopez <lopezemi269@gmail.com>
2025-11-28 00:11:32 -0300

    dsddsfef

--- src/Screens/WorkspaceDetailScreen/WorkspaceDetailScreen.jsx
+++ src/Screens/WorkspaceDetailScreen/WorkspaceDetailScreen.jsx
@@ -3,8 +3,6 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import useFetch from "../../hooks/useFetch";
 import { getWorkspaceById, getWorkspaceMembers } from "../../services/workspaceService";
-import InviteUserForm from "../../Components/InviteUserForm/InviteUserForm";
-import ChannelList from "../../components/ChannelList/ChannelList";
 
 const WorkspaceDetailScreen = () => {
   const { workspace_id } = useParams();
